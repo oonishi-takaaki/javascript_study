7d547c9968b0906dc33ec9ab10ceb558c066b468
takaaki <takaaking26@gmail.com>
2023-06-24 16:57:35 +0900

    駆け出し

---
+++ main.js
@@ -0,0 +1,27 @@
+'use strict';
+console.log('Hallo World');
+console.log('Hello! I\'m Takaaki Onishi');
+console.log('\\ \t AAA \n BBB');
+
+let color = 'Red';
+console.log(color);
+
+color = 'Green';
+console.log(color);
+
+const PI = 3.14;
+console.log(PI);
+
+console.log(typeof PI);
+
+let x = 3;
+let y = 2;
+
+let ans = Math.pow(x, y);
+console.log(ans);
+
+let weight = 86.7;
+let hight = 1.78;
+
+let BMI = weight / Math.pow(hight, 2);
+console.log(BMI);
